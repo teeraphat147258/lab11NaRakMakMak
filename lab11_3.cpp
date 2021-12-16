501d5e66c7f3f7081bbcfc4e2426e5caeb0b901b
teeraphat147258 <teeraphat147258@gmail.com>
2021-12-17 03:22:46 +0700

    lab11_3 completed

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,22 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    string score;
+    double N,Mean,sum=0,Zigma;
+    while(getline(source,score)){
+        sum += atof(score.c_str());
+        N++;
+        Zigma+=pow(atof(score.c_str()),2);
+    }
+    Mean = sum/N;
+    cout << "Number of data = "<< N <<"\n";;
+    cout << setprecision(3);
+    cout << "Mean = "<< sum/N<< "\n";
+    cout << "Standard deviation = "<<sqrt(((1/N)*Zigma)-pow(Mean,2));
+}
